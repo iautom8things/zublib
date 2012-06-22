5c38dc8e467bf05b6d010ef30d310cc65b1a4eaf
Manuel Zubieta <mazubieta@gmail.com>
2012-06-21 22:36:16 -0500

    added new output functions: record_data write_output and get_seperated_string

--- zublib/output.py
+++ zublib/output.py
@@ -1,4 +1,31 @@
-import sys
+import sys, os
+from time import time
+from datetime import datetime
+
+def get_seperated_string (seperator, *args ):
+    """return a string of each argument seperated by a tab char"""
+    return seperator.join(map(str, args))
+
+def record_data ( list, seperator, *args ):
+    """append information to DATA list"""
+    list.append( get_seperated_string(seperator,*args) )
+
+def write_output ( data, out_folder, name, link=False ):
+    add_newline = lambda x: str(x) + "\n"
+    # add newline characters to the end of each of the data items
+    data = map(add_newline, data)
+    # get a string representation of the date
+    outfile = name + "_" + str(datetime.fromtimestamp(time()))
+    out_path = os.path.join(out_folder, outfile)
+    # write the data to the log
+    with open(out_path, 'w') as f:
+        f.writelines(data)
+    if link:
+        # update symlink latest to point to latest
+        latest_path = os.path.join(out_folder, 'latest')
+        command = 'ln -fs "%s" "%s"' % (out_path, latest_path)
+        # execute the symlink command
+        os.system(command)
 
 def print_status ( pos, max_val, width=2, percentage=5 ):
     """Given two integers display a status bar that looks like:
